2dc46d934a6bec470f692547ce7a5ae729975a56
phbdias <phbdias25@gmail.com>
2024-05-01 23:51:18 +0100

    Added ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Work from "./components/Work";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 function App() {
-
   return (
     <div className="app">
-      <Header/>
-      <About/>
-      <Work/>
-      <Contact/>
+      <Header />
+      <About />
+      <Work />
+      <Contact />
+      <ScrollToTop />
     </div>
   );
 }
